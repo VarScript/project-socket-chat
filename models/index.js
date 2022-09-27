7c15d74ec7a6630bfbd3b0eccae510aedfcd9d09
DevHars <vargassanchezfabio01@gmail.com>
2022-09-27 17:19:14 -0500

    Model for management users connect

--- models/index.js
+++ models/index.js
@@ -1,13 +1,15 @@
-const Category = require('./category');
-const Product = require('./product');
-const Role = require('./role');
-const Server = require('./server');
-const User = require('./user');
+const Category   = require('./category');
+const ChatMessge = require('./chat-message');
+const Product    = require('./product');
+const Role       = require('./role');
+const Server     = require('./server');
+const User       = require('./user');
 
 
 
 module.exports = {
     Category,
+    ChatMessge,
     Product,
     Role,
     Server,
